8064890b3d63371158d748c79acc0f679a9d2321
satyasainath963 <59592754+satyasainath963@users.noreply.github.com>
2023-07-09 18:47:09 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <YOUR OWN GITHUB ACCOUNT>
+   # <satyasainath963>
    # Input:
    # p, principal amount
    # t, time period in years
